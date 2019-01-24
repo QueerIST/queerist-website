fa680d85a72f6f92fb9e2a58cff5a9bcd4b5e403
Francisco Sousa <f.sou@hotmail.com>
2019-01-24 13:46:58 +0000

    Updates

---
+++ precache-manifest.e3042786d641c0685a0cfe59e735ef48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/queerist-website/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "dd739c4a64372025727e",
+    "url": "/queerist-website/static/js/runtime~main.dd739c4a.js"
+  },
+  {
+    "revision": "68b5987bcfce9c4c4561",
+    "url": "/queerist-website/static/js/main.68b5987b.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/queerist-website/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "68b5987bcfce9c4c4561",
+    "url": "/queerist-website/static/css/main.9a031fdf.chunk.css"
+  },
+  {
+    "revision": "c117a3c947ac08fb42daf53f8f46f40e",
+    "url": "/queerist-website/index.html"
+  }
+];

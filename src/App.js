2220f435efb1cead0f3f834813fee10dcdce8624
Francisco Sousa <f.sou@hotmail.com>
2019-01-24 18:17:45 +0000

    :construction: Cria componentes base: Header, Page e Footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import {Header, Page, Footer} from './components'
 
 class App extends Component {
   render() {
     return (
       <div>
+        <Header></Header>
+        <Page></Page>
+        <Footer></Footer>
       </div>
     );
   }

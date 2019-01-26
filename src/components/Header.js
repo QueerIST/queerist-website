8688521c775a08a1a41f21c5015cb9cd35f332f1
Francisco Sousa <f.sou@hotmail.com>
2019-01-26 18:12:40 +0000

    :sparkles: Começa o header com logo e resizable para web

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,17 @@
 import React from 'react'
+import './components.css'
 
-const Header = () => <div>oi</div>
+function Header() {
+	return (
+		<div className="navbar">
+			<div className="navbar-container">
+			<a className="navimg"><img src="assets/logo so letras brancas.png"></img></a>
+			<a className="navlink">Eventos</a>
+			<a className="navlink">Projetos</a>
+			<a className="navlink">Merch</a>
+			</div>
+		</div>
+	)
+}
 
 export default Header

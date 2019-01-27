e759eec8b5e735e13094b921883cdef8fb30c6aa
Francisco Sousa <f.sou@hotmail.com>
2019-01-27 00:48:07 +0000

    Updates

---
+++ precache-manifest.61789410a1995a10ebd0741d2ab69d2b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dd739c4a64372025727e",
+    "url": "/queerist-website/static/js/runtime~main.dd739c4a.js"
+  },
+  {
+    "revision": "7863fa9dbeac06ca46da",
+    "url": "/queerist-website/static/js/main.7863fa9d.chunk.js"
+  },
+  {
+    "revision": "fbfa9ac1c04d6f818c50",
+    "url": "/queerist-website/static/js/1.fbfa9ac1.chunk.js"
+  },
+  {
+    "revision": "7863fa9dbeac06ca46da",
+    "url": "/queerist-website/static/css/main.71645f78.chunk.css"
+  },
+  {
+    "revision": "a57d99835f582dec1eba5dba4ad191f2",
+    "url": "/queerist-website/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/queerist-website/precache-manifest.35e98719c85d0550484444f660ab54c1.js"
+  "/queerist-website/precache-manifest.61789410a1995a10ebd0741d2ab69d2b.js"
 );
 
 workbox.clientsClaim();

35a6b2345dbcc7a7f58f37977a521b6e94386537
Francisco Sousa <f.sou@hotmail.com>
2019-01-27 00:45:11 +0000

    :iphone: Faz barra de navegação responsiva

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,36 @@
 import React from 'react'
 import './components.css'
 
-function Header() {
-	return (
-		<div className="navbar">
-			<div className="navbar-container">
-			<a className="navimg"><img src="assets/logo so letras brancas.png"></img></a>
-			<a className="navlink">Eventos</a>
-			<a className="navlink">Projetos</a>
-			<a className="navlink">Merch</a>
+class Header extends React.Component {
+	constructor() {
+		super()
+		this.state = { openMenu: false }
+		this.handleClickButton = this.handleClickButton.bind(this);
+	}
+
+	handleClickButton() {
+		this.setState({ openMenu: !this.state.openMenu })	
+	}
+
+	render() {
+		const classplusname = this.state.openMenu ? " navmenuopen" : ""
+		return (
+			<div className="navbar">
+				<div className="navbar-container">
+					<div className="navtop">
+						<i className="navtopside material-icons" onClick={this.handleClickButton}>menu</i>
+						<a className="navimg">
+							<img src="assets/logo so letras brancas.png"></img>
+						</a>
+						<div className="navtopside"></div>
+					</div>
+					<a className={"navlink" + classplusname}>Eventos</a>
+					<a className={"navlink" + classplusname}>Projetos</a>
+					<a className={"navlink" + classplusname}>Merch</a>
+				</div>
 			</div>
-		</div>
-	)
+		)
+	}
 }
 
 export default Header

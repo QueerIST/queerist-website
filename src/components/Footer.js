02b62d7147a5a63e15fc30665348586e04471994
Francisco Sousa <f.sou@hotmail.com>
2019-01-28 15:15:36 +0000

    :sparkles: Cria o footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,23 @@
 import React from 'react'
+import './footer.css'
+//import ReactSVG from '../react-svg.production.min.js'
 
-const Footer = () => <div></div>
+class Footer extends React.Component {
+
+	render() {
+		return (
+			<div className="footer">
+				<div className="footer-logos">
+					<img className="footer-logo" alt="facebook" src='assets/facebook.png'></img>
+					<img className="footer-logo" alt="instagram" src='assets/instagram.png'></img>
+					<img className="footer-logo" alt="twitter" src='assets/twitter.png'></img>
+					<img className="footer-logo" alt="github" src='assets/github.png'></img>
+					<img className="footer-logo" alt="email" src='assets/email.png'></img>
+				</div>
+				<div className="footer-text"></div>
+			</div>
+		)
+	}
+}
 
 export default Footer

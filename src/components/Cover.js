f700c28c69b0fc95df887f8fd4679b9f343fce0d
Francisco Sousa <f.sou@hotmail.com>
2019-01-28 00:11:31 +0000

    :bug: Corrigido o link da imagem de fundo

--- src/components/Cover.js
+++ src/components/Cover.js
@@ -4,7 +4,7 @@ import './cover.css'
 class Cover extends React.Component {
 	render() {
 		return (
-			<div className="background">
+			<div className="background" style={{backgroundImage: 'url(assets/fundo.png)'}}>
 				<img src="assets/Cores fundo claro.png" alt="QueerIST logo"></img>
 			</div>
 		)

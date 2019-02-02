bd72f5331b0af90aef3e5cab76b7a6c0426439d7
Francisco Sousa <f.sou@hotmail.com>
2019-02-02 22:25:46 +0000

    :alembic: Transforma as imagens do footer em svg's

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './footer.css'
-//import ReactSVG from '../react-svg.production.min.js'
+import ReactSVG from 'react-svg'
 
 class Footer extends React.Component {
 
@@ -8,11 +8,11 @@ class Footer extends React.Component {
 		return (
 			<div className="footer">
 				<div className="footer-logos">
-					<img className="footer-logo" alt="facebook" src='assets/facebook.png'></img>
-					<img className="footer-logo" alt="instagram" src='assets/instagram.png'></img>
-					<img className="footer-logo" alt="twitter" src='assets/twitter.png'></img>
-					<img className="footer-logo" alt="github" src='assets/github.png'></img>
-					<img className="footer-logo" alt="email" src='assets/email.png'></img>
+					<ReactSVG className="footer-logo" src="assets/facebook.svg" />
+					<ReactSVG className="footer-logo" src="assets/instagram.svg" />
+					<ReactSVG className="footer-logo" src="assets/twitter.svg" />
+					<ReactSVG className="footer-logo" src="assets/github.svg" />
+					<ReactSVG className="footer-logo" src="assets/email.svg" />
 				</div>
 				<div className="footer-text"></div>
 			</div>

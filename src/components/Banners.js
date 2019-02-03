f897034e958ac267c8fdd752f6c4de186ee90dd8
Francisco Sousa <f.sou@hotmail.com>
2019-02-03 15:56:59 +0000

    :art: Emancipa o texto dos banners pequenos

--- src/components/Banners.js
+++ src/components/Banners.js
@@ -9,11 +9,11 @@ function BigBanner() {
 	)
 }
 
-function SmallBanner({ link, name, bcolor, tcolor }) {
+function SmallBanner({ link, name, bcolor, tcolor, text, subtext }) {
 	return (
 		<div className="small-banner banner" data-aos="zoom-in" style={{ backgroundColor: bcolor, color: tcolor }}>
-			<h1 className="small-banner-text name">"Faz-te Ouvir"</h1>
-			<h2 className="small-banner-text clarification">Tertúlia</h2>
+			<h2 className="small-banner-text name">{text}</h2>
+			<h3 className="small-banner-text clarification">{subtext}</h3>
 			<div className="small-banner-img">
 				<img src={link} alt='' />
 			</div>
@@ -36,9 +36,24 @@ class Banners extends React.Component {
 			<React.Fragment>
 				<BigBanner />
 				<SmallBanners>
-					<SmallBanner link='assets/ban1.png' bcolor='#6bcde7' tcolor='white' />
-					<SmallBanner link='assets/ban2.png' bcolor='white' tcolor='black' />
-					<SmallBanner link='assets/ban3.png' bcolor='#232323' tcolor='white' />
+					<SmallBanner
+						link='assets/ban1.png'
+						bcolor='#6bcde7'
+						tcolor='white'
+						text='"Faz-te Ouvir"'
+						subtext="Tertúlia" />
+					<SmallBanner
+						link='assets/ban2.png'
+						bcolor='white'
+						tcolor='black'
+						text=''
+						subtext="PubCrawl" />						
+					<SmallBanner
+						link='assets/ban3.png'
+						bcolor='#232323'
+						tcolor='white'
+						text='Zero Preconceitos'
+						subtext="Podcast" />
 				</SmallBanners>
 			</React.Fragment>
 		)

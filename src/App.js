9ef6d624a35dbd9246de4ddac817b93aa6eb88b5
Francisco Sousa <f.sou@hotmail.com>
2019-02-03 16:22:17 +0000

    :bug: Corrige bug de dar deploy para endereço não root

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom'
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <React.Fragment>
           <Header />
           <Page />

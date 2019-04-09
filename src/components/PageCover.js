4f15060fc29c0bcaa97608b30db74bd885657322
Francisco Sousa <francisco.sousa.98@tecnico.ulisboa.pt>
2019-04-09 16:14:57 +0100

    :art: Separa a Page Cover em duas componentes
    Reduz tamanho de uma imagem

--- src/components/PageCover.js
+++ src/components/PageCover.js
@@ -1,17 +1,29 @@
 import React from 'react'
 import './pagecover.css'
 
+function PageBackground() {
+	return (
+		<div className="page-background">
+			<h1>Eventos</h1>
+			<p>Onde te podes divertir e conviver e aprender e informar e educar e tudo e mais. Vem fazer coisas connosco!</p>
+		</div>
+	)
+}
+
+function PageImage() {
+	return (
+		<div className="page-image">
+			<img src="assets/tertulia.jpg" />
+		</div>
+	)
+}
+
 class PageCover extends React.Component {
 	render() {
 		return (
 			<React.Fragment>
-				<div className="page-background">
-					<h1>Eventos</h1>
-					<p>Onde te podes divertir e conviver e aprender e informar e educar e tudo e mais. Vem fazer coisas connosco!</p>
-				</div>
-				<div className="page-image">
-					<img src="assets/tertulia.jpg" />
-				</div>
+				<PageBackground />
+				<PageImage />
 			</React.Fragment>
 		)
 	}

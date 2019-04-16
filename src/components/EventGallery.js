9cd3ff440c3c0697854c27f8358a3a9a1168273a
Francisco Sousa <francisco.sousa.98@tecnico.ulisboa.pt>
2019-04-16 21:53:53 +0100

    :construction: Coloca texto e link para o facebook nos eventos

--- src/components/EventGallery.js
+++ src/components/EventGallery.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './eventgallery.css'
+import { ReactComponent as Launch } from './../svg/launch.svg'
 
 const EventGalleryWrap = ({ children }) => (
 	<ul className="event-gallery">
@@ -7,7 +8,8 @@ const EventGalleryWrap = ({ children }) => (
 	</ul>
 )
 
-function EventGalleryItem() {
+class EventGalleryItem extends React.Component {
+	render() {
 	return (
 		<li className="event-gallery-item">
 			<div className="event-gallery-item-img">
@@ -15,15 +17,19 @@ function EventGalleryItem() {
 			</div>
 			<div className="event-gallery-item-text">
 				<h3>Faz-te Ouvir!</h3>
-				<h4>22-11</h4>
+					<span className="event-gallery-item-launch">
+						<h4>22 nov, 18h @ GA1</h4>
+						<a href="https://www.facebook.com/events/1074556752718680/"><Launch /></a>
+					</span>
 				<p>Sabemos que ao longo dos anos o conceito de família tem evoluído mas
 					de que forma? Quão diferente é a dinâmica familiar de um agregado
-					com pais queer? 👩‍👩‍👧 👨‍👨‍👦 E como é que pais não queer lidam com as necessidades
+						com pais queer? E como é que pais não queer lidam com as necessidades
 					dos seus filhos queer? Quais são as suas dificuldades e desafios?
 				</p>
 			</div>
 		</li>
 	)
+}
 }
 
 class EventGallery extends React.Component {

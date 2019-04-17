20e5c6912a91fcde79047c498c0280d3db9ee47b
Francisco Sousa <francisco.sousa.98@tecnico.ulisboa.pt>
2019-04-17 17:44:14 +0100

    :heavy_plus_sign: Adiciona dependência react-snapshot

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { render } from 'react-snapshot';
 import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
@@ -11,7 +11,7 @@ AOS.init({
 	once: true
 });
 
-ReactDOM.render(<App />, document.getElementById('root'))
+render(<App />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

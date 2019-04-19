0c2ce21b0f53f1f445ea68865fb489c0ecd5c67b
Francisco Sousa <francisco.sousa.98@tecnico.ulisboa.pt>
2019-04-19 02:42:54 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/queerist-website/precache-manifest.34a86c40d1830220b490594d2ad3e249.js"
+  "/queerist-website/precache-manifest.c24b65d0e05cc5cd45b456f3a6978eba.js"
 );
 
 workbox.clientsClaim();

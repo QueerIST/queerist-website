077f31c093f236d220a8612c14482f717883e929
Francisco Sousa <francisco.sousa.98@tecnico.ulisboa.pt>
2019-06-26 16:47:55 +0100

    :children_crossing: Header esconde-se no scroll

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import Headroom from 'react-headroom'
 import './header.css'
 
 class Header extends React.Component {
@@ -21,22 +22,24 @@ class Header extends React.Component {
 	render() {
 		const menustatus = this.state.openMenu ? " navmenu-open" : ""
 		return (
-			<div className="navbar">
-				<div className="navbar-content">
-					<div className="navbar-delimiter">
-						<div className="navtop">
-							<div className="navtop-side"></div>
-							<NavLink className="navimg" exact to="/" onClick={this.handleClickLink}>
-								<img src="assets/logo so letras brancas.png" alt="QueerIST logo" />
-							</NavLink>
-							<i className="navtop-side material-icons" onClick={this.handleClickMenuButton}>menu</i>
+			<Headroom>
+				<div className="navbar">
+					<div className="navbar-content">
+						<div className="navbar-delimiter">
+							<div className="navtop">
+								<div className="navtop-side"></div>
+								<NavLink className="navimg" exact to="/" onClick={this.handleClickLink}>
+									<img src="assets/logo so letras brancas.png" alt="QueerIST logo" />
+								</NavLink>
+								<i className="navtop-side material-icons" onClick={this.handleClickMenuButton}>menu</i>
+							</div>
+							<NavLink className={"navlink" + menustatus} to="/events" onClick={this.handleClickLink}>Eventos</NavLink>
+							<NavLink className={"navlink" + menustatus} to="/projects" onClick={this.handleClickLink}>Projetos</NavLink>
+							<NavLink className={"navlink" + menustatus} to="/merch" onClick={this.handleClickLink}>Merch</NavLink>
 						</div>
-						<NavLink className={"navlink" + menustatus} to="/events" onClick={this.handleClickLink}>Eventos</NavLink>
-						<NavLink className={"navlink" + menustatus} to="/projects" onClick={this.handleClickLink}>Projetos</NavLink>
-						<NavLink className={"navlink" + menustatus} to="/merch" onClick={this.handleClickLink}>Merch</NavLink>
 					</div>
 				</div>
-			</div>
+			</Headroom>
 		)
 	}
 }

746541225a09421f2493ab3adff19caa259250f9
Francisco Sousa <f.sou@hotmail.com>
2020-07-14 12:19:49 +0100

    :sparkles: Adiciona PageCover mas para subpáginas

--- src/components/PageCover.js
+++ src/components/PageCover.js
@@ -1,19 +1,31 @@
 import React from 'react'
 import './pagecover.css'
 
-function PageBackground({ title, description, bgColor }) {
+function PageBackground({ title, description, bgColor, imgBgColor }) {
 	return (
 		<div className="page-background" style={{ backgroundColor: bgColor }}>
 			<h1>{title}</h1>
-			<p>{description}</p>
+			{!imgBgColor && <p>{description}</p>}
 		</div>
 	)
 }
 
-function PageImage({ imgLink, name }) {
+function PageImage({ imgLink, name, description, imgBgColor }) {
 	return (
 		<div className="page-image">
-			<img src={imgLink} alt={name} />
+			{
+				imgBgColor ?
+					<div className="page-image-group" >
+						<div className="page-image-child" >
+							<img src={imgLink} alt={name} />
+						</div>
+						<p className="page-image-child" style={{ backgroundColor: imgBgColor }}>{description}</p>
+					</div>
+					:
+					<div className="page-image-group" >
+						<img src={imgLink} alt={name} />
+					</div>
+			}
 		</div>
 	)
 }
@@ -24,11 +36,14 @@ class PageCover extends React.Component {
 			<React.Fragment>
 				<PageBackground
 					title={this.props.data.title}
+					imgBgColor={this.props.data.img_bg_color}
 					description={this.props.data.description}
 					bgColor={this.props.data.bg_color}
 				/>
 				<PageImage
 					name={this.props.data.title}
+					imgBgColor={this.props.data.img_bg_color}
+					description={this.props.data.description}
 					imgLink={this.props.data.img_link}
 				/>
 			</React.Fragment>

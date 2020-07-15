8b75a23f91329d9dddfb9fdd9485500a8e075583
Francisco Sousa <f.sou@hotmail.com>
2020-07-15 12:35:31 +0100

    :sparkles: Adapta EventTile para também permitir listar subpáginas

--- src/components/EventTile.js
+++ src/components/EventTile.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import { EventGallery, Button } from '.'
 import { publicPath } from '../helpers'
+import { NavLink } from 'react-router-dom'
 import { ReactComponent as Expand } from './../svg/expand.svg'
+// import { ReactComponent as Launch } from './../svg/launch.svg'
 
 import './eventtile.css'
 
@@ -17,9 +19,26 @@ class EventTypeInfo extends React.Component {
 	}
 
 	render() {
-		const { n, name, description, imgLink, bgColor, textColor, happenings, seeMoreText } = this.props;
+		const { n, id, parentPage, name, description, imgLink, bgColor, textColor, happenings, seeMoreText } = this.props;
 		const dir = n % 2 ? "left" : "right";
 		const openClass = this.state.open ? "open" : "";
+		const EventTypeInfoButton = () => parentPage ? (
+			<NavLink
+				className="event-type-info-b"
+				to={{
+					pathname: `${parentPage}/${id}`
+				}}>
+				{seeMoreText}
+				{/* <Launch fill={textColor} /> */}
+			</NavLink>
+		) : (
+				<button
+					onClick={this.toggleGallery}
+					className={`event-type-info-b event-type-info-button ${openClass}`}>
+					{seeMoreText}
+					<Expand fill={textColor} />
+				</button>
+			)
 		return (
 			<div className={`event-type-info ${dir}`} style={{
 				backgroundColor: bgColor, color: textColor
@@ -27,26 +46,25 @@ class EventTypeInfo extends React.Component {
 				<div className={`event-type-info-text ${openClass}`}>
 					<h3 className="event-type-info-text-text">{name}</h3>
 					<p className="event-type-info-text-desc event-type-info-text-text">{description}</p>
-					<Button>
-						<button onClick={this.toggleGallery} className={`event-type-info-button ${openClass}`}>
-							{dir === "right" ? seeMoreText : null}
-							<Expand fill={textColor} />
-							{dir === "left" ? seeMoreText : null}
-						</button>
+					<Button borderColor={textColor} color={textColor}>
+						{EventTypeInfoButton()}
 					</Button>
 				</div>
 				<div className="event-type-info-img">
 					<img className={openClass} src={publicPath(imgLink)} alt={name} />
+					{happenings && <EventGallery open={this.state.open} data={happenings} />}
 				</div>
 			</div>
 		)
 	}
 }
 
-const EventTile = ({ n, data }) => (
+const EventTile = ({ n, data, parentPage }) => (
 	<div id={data.id}>
 		<EventTypeInfo
 			n={n}
+			id={data.id}
+			parentPage={parentPage}
 			name={data.name}
 			description={data.description}
 			imgLink={data.img_link}

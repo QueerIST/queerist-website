2f681202ad2124997557f62ebf24402c46367c2a
Francisco Sousa <f.sou@hotmail.com>
2020-08-08 14:54:17 +0100

    :recycle: Passa a usar o Button no TextBlock e permite personalização

--- src/components/TextBlock.js
+++ src/components/TextBlock.js
@@ -1,25 +1,31 @@
 import React from 'react'
+import { Button } from '.'
 import { NavHashLink as NavLink } from 'react-router-hash-link'
 import { scrollOptions } from '../helpers'
 
 import './textblock.css'
 
-const TextBlockInfo = ({ id, title, text, bgColor, textColor, linkText, linkPage, linkId }) => (
+const TextBlockInfo = ({ id, title, text, bgColor, textColor, linkBackgroundColor, linkText, linkPage, linkId }) => (
 	<div id={id} className="textblock" style={{ backgroundColor: bgColor, color: textColor }}>
 		<div className="textblock-child">
 			<h2 className="textblock-button-child textblock-button-text">{title}</h2>
 			{linkText &&
 				<div className="textblock-button-child">
-					{/* Kinda meh*/}
-					<NavLink className="textblock-button"
-						color={textColor}
-						to={{
-							pathname: linkPage, hash: '#' + linkId
-						}}
-						scroll={scrollOptions}
+					<Button
+						block
+						color={bgColor}
+						backgroundColor={linkBackgroundColor}
 					>
-						{linkText}
-					</NavLink>
+						<NavLink
+							className="textblock-button"
+							to={{
+								pathname: linkPage, hash: '#' + linkId
+							}}
+							scroll={scrollOptions}
+						>
+							{linkText}
+						</NavLink>
+					</Button>
 				</div>}
 		</div>
 		<p className="textblock-child textblock-text">{text}</p>
@@ -33,6 +39,7 @@ const TextBlock = ({ data }) => (
 		text={data.text}
 		bgColor={data.bg_color}
 		textColor={data.text_color}
+		linkBackgroundColor={data.link_bg_color}
 		linkText={data.link_text}
 		linkPage={data.link_page}
 		linkId={data.link_id}

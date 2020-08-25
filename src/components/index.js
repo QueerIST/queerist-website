3984cb22cd1d80d74e065551d2b68df9452af8f2
Francisco Sousa <f.sou@hotmail.com>
2020-08-25 12:36:38 +0100

    :bug: Adiciona import do IconList na pasta dos componentes

--- src/components/index.js
+++ src/components/index.js
@@ -10,5 +10,6 @@ import Tile from './Tile'
 import Separator from './Separator'
 import EventGallery from './EventGallery'
 import Button from './Button'
+import IconList from './IconList'
 
-export { Header, Page, Footer, MainCover, TextBlock, Banners, Pages, PageCover, Tile, Separator, EventGallery, Button }
+export { Header, Page, Footer, MainCover, TextBlock, Banners, Pages, PageCover, Tile, Separator, EventGallery, Button, IconList }

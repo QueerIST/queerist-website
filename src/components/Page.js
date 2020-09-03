e946e59b5c4a3cbb5dae7682c201d5cdd473b6b6
Francisco Sousa <f.sou@hotmail.com>
2020-09-03 18:03:03 +0100

    :mag: Faz meta tags para SEO e redes socias
    Usando o Helmet para adicionar e os novos helpers para fazer caminhos absolutos

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,9 +1,38 @@
 import React from 'react'
+import { fullPath, fullPathSlashless } from '../helpers'
+import { Helmet } from 'react-helmet'
+import { withRouter } from 'react-router'
 
-const Page = ({ children }) => (
+const Page = ({ data, location, children }) => (
 	<div /*data-aos="fade-left"*/>
+		<Helmet
+			defaultTitle="QueerIST – Incluir, informar, dialogar"
+			titleTemplate="%s – QueerIST"
+			defer={false}
+		>
+			{/* HTML Meta Tags */}
+			{!data.home && <title>{data.name}</title>}
+			<meta name="description" content={data.description} />
+
+			{/* Google / Search Engine Tags */}
+			<meta itemprop="name" content={data.name} />
+			<meta itemprop="description" content={data.description} />
+			<meta itemprop="image" content={fullPath(data.img_link)} />
+
+			{/* Facebook Meta Tags */}
+			<meta property="og:url" content={fullPathSlashless(location.pathname)} />
+			<meta property="og:type" content="website" />
+			<meta property="og:title" content={data.name} />
+			<meta property="og:description" content={data.description} />
+			<meta property="og:image" content={fullPath(data.img_link)} />
+
+			{/* Twitter Meta Tags */}
+			<meta name="twitter:site" content="@queerist" />
+			<meta name="twitter:card" content="summary_large_image" />
+			{/* <meta name="twitter:card" content="summary" /> */}
+		</Helmet>
 		{children}
 	</div>
 )
 
-export default Page
+export default withRouter(Page)

07a82b4d7a14ba55576764f27d403c627ce8e949
Francisco Sousa <f.sou@hotmail.com>
2020-09-15 11:23:36 +0100

    :construction: Atualiza estrutura dos blocos da SubPage

--- src/pages/SubPage.js
+++ src/pages/SubPage.js
@@ -1,18 +1,25 @@
 import React from 'react'
-import { Page, PageCover, Tile, Separator, TextBlock } from '../components'
+import { Page, PageCover, Tile, Separator, TextBlock, IconList, HighlightBox } from '../components'
 
 const SubPage = ({ id, parentData }) => {
 	const data = parentData.sub_pages.find(p => p.id === id)
 	return (
-		<Page>
-			<PageCover parentPage={parentData.name} data={data} />
-			<Separator />
+		<Page data={data}>
+			<PageCover data={data} isSubPage />
+			<Separator data={data.separator} />
+			{data.icons && <IconList data={data.icons} />}
 			<TextBlock data={data.text_block} />
-			{data.events && data.events.map(
-				(event, i) => (
-					<Tile key={i} n={i} data={event} />
-				)
-			)}
+			{data.highlightbox && <HighlightBox data={data.highlightbox} />}
+			{data.events && <>
+				<Separator data={data.separator_events} />
+				{data.events.map(
+					(event, i) => (
+						<Tile key={i} n={i} data={event} />
+					)
+				)}
+			</>
+			}
+
 		</Page>
 	)
 }

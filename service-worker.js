b019948ecc86054f5eda9c80e5e2e526fa2cb641
Francisco Sousa <f.sou@hotmail.com>
2020-09-17 12:28:15 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://queerist.github.io/queerist-website/precache-manifest.4b38c0a429c2ceceb320f3c5f21d8d24.js"
+  "/queerist-website/precache-manifest.50018c683c2908d687ec2b9135665d8f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://queerist.github.io/queerist-website/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/queerist-website/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });

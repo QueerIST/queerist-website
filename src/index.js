0ccd898fc08e2964be747f2ade37a501211bc849
Francisco Sousa <francisco.sousa.98@tecnico.ulisboa.pt>
2023-12-29 12:22:21 +0000

    :fire: Remove serviceWorker e teste

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { render } from 'react-snapshot';
 import './index.css'
 import App from './App'
-import * as serviceWorker from './serviceWorker'
 import AOS from 'aos'
 import ReactGA from 'react-ga4'
 import { createBrowserHistory } from 'history'
@@ -30,7 +29,3 @@ analyticsHistory(history.location)
 history.listen(analyticsHistory)
 
 render(<App history={history} />, document.getElementById('root'))
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister()

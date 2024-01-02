e80d3f1163d3270639fe51683d53106c3e16d45b
Francisco Sousa <francisco.sousa.98@tecnico.ulisboa.pt>
2024-01-02 23:37:02 +0000

    :bug: Corrige GA_CODE vazio

--- src/index.js
+++ src/index.js
@@ -1,23 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
 import './index.css'
-import App from './App'
 import AOS from 'aos'
 import ReactGA from 'react-ga4'
+
+import App from './App'
 import 'aos/dist/aos.css'
 import './webfontkit/fontstylesheet.css'
 
-
 AOS.init({
-	once: true
-});
+  once: true
+})
 
-if (import.meta.env.PROD) {
-	ReactGA.initialize(import.meta.env.VITE_GA_CODE, {
-		gaOptions: {
-			siteSpeedSampleRate: 100
-		}
-	})
+if (import.meta.env.PROD && import.meta.env.MODE !== 'github') {
+  ReactGA.initialize(import.meta.env.VITE_GA_CODE, {
+    gaOptions: {
+      siteSpeedSampleRate: 100
+    }
+  })
 }
 
 ReactDOM.createRoot(document.getElementById('root')).render(

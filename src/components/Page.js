ca856286a571460aae0ed702bb018abb5742f54c
Francisco Sousa <francisco.sousa.98@tecnico.ulisboa.pt>
2024-01-06 18:48:24 +0000

    :arrow_up: Troca o react-helmet pelo react-helmet-async

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,42 +1,52 @@
 import React from 'react'
-import { fullPath, fullPathSlashless } from '../helpers'
-import { Helmet } from 'react-helmet'
+
+import { Helmet, HelmetData } from 'react-helmet-async'
 import { useLocation } from 'react-router-dom'
 
+import { fullPath, fullPathSlashless } from '../helpers'
+
 const Page = ({ data, children }) => {
-	const location = useLocation()
-	return (
-		<div /*data-aos="fade-left"*/>
-			<Helmet
-				defaultTitle="QueerIST – Incluir, informar, dialogar"
-				titleTemplate="%s – QueerIST"
-				defer={false}
-			>
-				{/* HTML Meta Tags */}
-				{!data.home && <title>{data.name}</title>}
-				<meta name="description" content={data.description} />
-				<link rel="canonical" href={fullPathSlashless(location.pathname)} />
+  const location = useLocation()
+  const helmetData = new HelmetData({})
+  return (
+    <div>
+      <Helmet
+        defaultTitle='QueerIST – Incluir, informar, dialogar'
+        titleTemplate='%s – QueerIST'
+        helmetData={helmetData}
+        defer={false}
+      >
+        {/* HTML Meta Tags */}
+        {!data.home && <title>{data.name}</title>}
+        <meta name='description' content={data.description} />
+        <link
+          rel='canonical'
+          href={fullPathSlashless(location.pathname)}
+        />
 
-				{/* Google / Search Engine Tags */}
-				<meta itemprop="name" content={data.name} />
-				<meta itemprop="description" content={data.description} />
-				<meta itemprop="image" content={fullPath(data.img_link)} />
+        {/* Google / Search Engine Tags */}
+        <meta itemprop='name' content={data.name} />
+        <meta itemprop='description' content={data.description} />
+        <meta itemprop='image' content={fullPath(data.img_link)} />
 
-				{/* Facebook Meta Tags */}
-				<meta property="og:url" content={fullPathSlashless(location.pathname)} />
-				<meta property="og:type" content="website" />
-				<meta property="og:title" content={data.name} />
-				<meta property="og:description" content={data.description} />
-				<meta property="og:image" content={fullPath(data.img_link)} />
+        {/* Facebook Meta Tags */}
+        <meta
+          property='og:url'
+          content={fullPathSlashless(location.pathname)}
+        />
+        <meta property='og:type' content='website' />
+        <meta property='og:title' content={data.name} />
+        <meta property='og:description' content={data.description} />
+        <meta property='og:image' content={fullPath(data.img_link)} />
 
-				{/* Twitter Meta Tags */}
-				<meta name="twitter:site" content="@queerist" />
-				<meta name="twitter:card" content="summary_large_image" />
-				{/* <meta name="twitter:card" content="summary" /> */}
-			</Helmet>
-			{children}
-		</div>
-	)
+        {/* Twitter Meta Tags */}
+        <meta name='twitter:site' content='@queerist' />
+        <meta name='twitter:card' content='summary_large_image' />
+        {/* <meta name="twitter:card" content="summary" /> */}
+      </Helmet>
+      {children}
+    </div>
+  )
 }
 
 export default Page

3c537bd94e7deafb06324ba36f32398c591ee48b
Francisco Sousa <francisco.sousa.98@tecnico.ulisboa.pt>
2024-01-09 22:41:06 +0000

    :children_crossing: Começa a tipificar a página Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import { MainCover, TextBlock, BigBanner, SmallBanners, Page, HighlightBox } from '../components'
+import { type DMainPage } from '../types/data'
 
-const Home = ({ data }) => (
+const Home = ({ data }: { data: DMainPage }) => (
   <Page data={data}>
     <MainCover />
     <TextBlock data={data.text_block} />

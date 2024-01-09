b86dc6a41dcdd2d0e2b72395f32eecf936f2e960
Francisco Sousa <francisco.sousa.98@tecnico.ulisboa.pt>
2024-01-09 22:38:02 +0000

    :label: Cria os tipos de dados e dominio

---
+++ src/types/data.ts
@@ -0,0 +1,175 @@
+export interface Data {
+  main_page: DMainPage
+  about_page: DAboutPage
+  events_page: DEventsPage
+  projects_page: DProjectsPage
+}
+
+export interface DMainPage {
+  home: boolean
+  name: string
+  description: string
+  img_link: string
+  text_block: DTextBlock
+  banners: DBanners
+  highlightbox: DHighlightbox
+}
+
+export interface DBaseTextBlock {
+  id: string
+  title: string
+  text: string | string[]
+  bg_color: string
+  title_color?: string
+  text_color: string
+}
+
+interface DBaseTextBlockWithLink extends DBaseTextBlock {
+  link_text: string
+  link_bg_color?: string
+  link_text_color?: string
+}
+
+export interface DLinkToPage extends DBaseTextBlockWithLink {
+  link_page: string
+  link_id: string
+}
+
+export interface DLinkToFile extends DBaseTextBlockWithLink {
+  link_file: string
+}
+
+export interface DLinkToWeb extends DBaseTextBlockWithLink {
+  link_web: string
+}
+
+export type DTextBlockWithLink = DLinkToPage | DLinkToFile | DLinkToWeb
+
+export type DTextBlock = DBaseTextBlock | DTextBlockWithLink
+
+export interface DBanners {
+  big_banner: DBigBanner
+  small_banners?: DSmallBanner[]
+}
+
+export interface DBigBanner {
+  name: string
+  img_link: string
+  text_color?: string
+  link_text?: string
+  link_page?: string
+  link_id?: string
+}
+
+export interface DSmallBanner {
+  name: string
+  label: string
+  logo_link: string
+  bg_color: string
+  text_color: string
+  link_text: string
+  link_page: string
+  link_id: string
+}
+
+export interface DHighlightbox {
+  id?: string
+  title: string
+  sub_title: string
+  bg_color: string
+  text_color: string
+  link_text_color: string
+  link_bg_color: string
+  link_text: string
+  link_web: string
+}
+
+export interface DAboutPage {
+  id: string
+  name: string
+  description: string
+  img_link: string
+  bg_color: string
+  separator: string
+  text_block_1: DTextBlock
+  text_block_2: DTextBlock
+  banners: DBanners
+  textboxs: DTextboxs
+}
+
+export interface DTextboxs {
+  id: string
+  boxes: DBox[]
+}
+
+export interface DBox {
+  name: string
+  bg_color: string
+  text: string
+}
+
+export interface DEventsPage {
+  name: string
+  description: string
+  img_link: string
+  bg_color: string
+  events: DEvent[]
+  highlightbox: DHighlightbox
+}
+
+export interface DEvent {
+  id: string
+  name: string
+  description: string
+  see_more_text?: string
+  img_link: string
+  logo_link?: string
+  bg_color: string
+  text_color: string
+  happenings?: DHappening[]
+}
+
+export interface DHappening {
+  name: string
+  img_link: string
+  date: string
+  enddate?: string
+  place: string
+  time?: string
+  link: string
+  description?: string
+}
+
+export interface DProjectsPage {
+  id: string
+  name: string
+  description: string
+  img_link: string
+  bg_color: string
+  sub_pages: DSubPage[]
+}
+
+export interface DSubPage {
+  id: string
+  name: string
+  see_more_text: string
+  description: string
+  img_link: string
+  logo_link?: string
+  img_bg_color: string
+  bg_color: string
+  text_color: string
+  text_block: DTextBlock
+  separator_events?: string
+  events?: DEvent[]
+  separator?: string
+  icons?: DIcon[]
+  highlightbox?: DHighlightbox
+  text_block_2?: DTextBlock
+}
+
+export interface DIcon {
+  name: string
+  logo_link?: string
+  link?: string
+}
---
+++ src/types/domain.ts
@@ -0,0 +1,175 @@
+export interface Domain {
+  mainPage: MainPage
+  aboutPage: AboutPage
+  eventsPage: EventsPage
+  projectsPage: ProjectsPage
+}
+
+export interface MainPage {
+  home: boolean
+  name: string
+  description: string
+  imgLink: string
+  textBlock: TextBlock
+  banners: Banners
+  highlightbox: Highlightbox
+}
+
+interface BaseTextBlock {
+  id: string
+  title: string
+  text: string | string[]
+  bgColor: string
+  titleColor?: string
+  textColor: string
+  small: boolean
+}
+
+interface BaseTextBlockWithLink extends BaseTextBlock {
+  linkText: string
+  linkTextColor?: string
+  linkBackgroundColor?: string
+}
+
+export interface LinkToPage extends BaseTextBlockWithLink {
+  linkPage: string
+  linkId: string
+}
+
+export interface LinkToFile extends BaseTextBlockWithLink {
+  linkFile: string
+}
+
+export interface LinkToWeb extends BaseTextBlockWithLink {
+  linkWeb: string
+}
+
+export type TextBlockWithLink = LinkToPage | LinkToFile | LinkToWeb
+
+export type TextBlock = BaseTextBlock | TextBlockWithLink
+
+export interface Banners {
+  bigBanner: BigBanner
+  smallBanners?: SmallBanner[]
+}
+
+export interface BigBanner {
+  name: string
+  imgLink: string
+  textColor?: string
+  linkText?: string
+  linkPage?: string
+  linkId?: string
+}
+
+export interface SmallBanner {
+  name: string
+  label: string
+  logoLink: string
+  bgColor: string
+  textColor: string
+  linkText: string
+  linkPage: string
+  linkId: string
+}
+
+export interface Highlightbox {
+  id?: string
+  title: string
+  subTitle: string
+  bgColor: string
+  textColor: string
+  linkTextColor: string
+  linkBgColor: string
+  linkText: string
+  linkWeb: string
+}
+
+export interface AboutPage {
+  id: string
+  name: string
+  description: string
+  imgLink: string
+  bgColor: string
+  separator: string
+  textBlock_1: TextBlock
+  textBlock_2: TextBlock
+  banners: Banners
+  textboxs: Textboxs
+}
+
+export interface Textboxs {
+  id: string
+  boxes: Box[]
+}
+
+export interface Box {
+  name: string
+  bgColor: string
+  text: string
+}
+
+export interface EventsPage {
+  name: string
+  description: string
+  imgLink: string
+  bgColor: string
+  events: Event[]
+  highlightbox: Highlightbox
+}
+
+export interface Event {
+  id: string
+  name: string
+  description: string
+  seeMoreText?: string
+  imgLink: string
+  logoLink?: string
+  bgColor: string
+  textColor: string
+  happenings?: Happening[]
+}
+
+export interface Happening {
+  name: string
+  imgLink: string
+  date: string
+  enddate?: string
+  place: string
+  time?: string
+  link: string
+  description?: string
+}
+export interface ProjectsPage {
+  id: string
+  name: string
+  description: string
+  imgLink: string
+  bgColor: string
+  subPages: SubPage[]
+}
+
+export interface SubPage {
+  id: string
+  name: string
+  seeMoreText: string
+  description: string
+  imgLink: string
+  logoLink?: string
+  imgBgColor: string
+  bgColor: string
+  textColor: string
+  textBlock: TextBlock
+  separatorEvents?: string
+  events?: Event[]
+  separator?: string
+  icons?: Icon[]
+  highlightbox?: Highlightbox
+  textBlock_2?: TextBlock
+}
+
+export interface Icon {
+  name: string
+  logoLink?: string
+  link?: string
+}

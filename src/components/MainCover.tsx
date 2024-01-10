b9adcf796d29b01a20a56852c828848d0848e722
Francisco Sousa <francisco.sousa.98@tecnico.ulisboa.pt>
2024-01-10 23:56:07 +0000

    :label: Mete tipificação na Main Cover

--- src/components/MainCover.tsx
+++ src/components/MainCover.tsx
@@ -1,18 +1,13 @@
-import React from 'react'
-
-import logo from '../img/Cores fundo claro.png'
+import logo from ' ../img/Cores fundo claro.png'
 import fundo from '../img/fundo.png'
+
 import './maincover.css'
 
-class MainCover extends React.Component {
-  render () {
-    return (
-      <div className='background' style={{ backgroundImage: `url(${fundo})` }}>
-        <img src={logo} alt='QueerIST logo' />
-        <h3>Secção Autónoma da Associação dos Estudantes do Instituto Superior Técnico</h3>
-      </div>
-    )
-  }
-}
+const MainCover = () => (
+  <div className='background' style={{ backgroundImage: `url(${fundo})` }}>
+    <img src={logo} alt='QueerIST logo' />
+    <h3>Secção Autónoma da Associação dos Estudantes do Instituto Superior Técnico</h3>
+  </div>
+)
 
 export default MainCover

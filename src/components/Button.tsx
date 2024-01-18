b751fe783f13af011273bfb9b7b5286933d1d062
Francisco Sousa <francisco.sousa.98@tecnico.ulisboa.pt>
2024-01-18 00:24:09 +0000

    :label: Tipifica o Button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,33 +1,45 @@
-import React from 'react'
+import { type PropsWithChildren, cloneElement, type ReactElement, isValidElement, Children, type HTMLAttributes } from 'react'
 
 import classNames from 'classnames'
 import ReactGA from 'react-ga4'
 
 import './button.css'
 
-export default class Button extends React.Component {
-  render () {
-    const { children, borderColor, color, backgroundColor, block, actionComp, actionName, actionLabel } = this.props
-    const buildClassName = (child) => classNames(
-      child.props.className,
-      'block-button',
-      { block }
-    )
-    const onClickAnalytics = (child) => {
-      ReactGA.event({
-        category: actionComp, // Required
-        action: actionName, // Required
-        label: actionLabel
-      })
-      child.props.onClick?.()
-    }
+type HTMLProps = HTMLAttributes<HTMLElement>
 
-    return React.Children.map(children, (child) => (
-      React.cloneElement(child, {
-        className: buildClassName(child),
-        style: { borderColor, color, backgroundColor },
-        onClick: () => { onClickAnalytics(child) }
-      })
-    ))
+interface ButtonProps {
+  color?: string
+  borderColor?: string
+  backgroundColor?: string
+  block?: boolean
+  actionComp: string
+  actionName: string
+  actionLabel?: string
+}
+
+export default function Button (props: PropsWithChildren<ButtonProps>) {
+  const { children, borderColor, color, backgroundColor, block, actionComp, actionName, actionLabel } = props
+
+  const buildClassName = (child: ReactElement<HTMLProps>) => classNames(
+    child.props.className,
+    'block-button',
+    { block }
+  )
+  const onClickAnalytics = (child: ReactElement) => {
+    ReactGA.event({
+      category: actionComp, // Required
+      action: actionName, // Required
+      label: actionLabel
+    })
+    child.props.onClick?.()
   }
+
+  return isValidElement<HTMLProps>(children) &&
+   Children.map(children, (child: ReactElement<HTMLProps>) => (
+     cloneElement(child, {
+       className: buildClassName(child),
+       style: { borderColor, color, backgroundColor },
+       onClick: () => { onClickAnalytics(child) }
+     })
+   ))
 }

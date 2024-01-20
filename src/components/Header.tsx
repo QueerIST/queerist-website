9bfffa150f73c8e49c79a873c3ba60e397cb2730
Francisco Sousa <francisco.sousa.98@tecnico.ulisboa.pt>
2024-01-20 13:49:45 +0000

    :pencil2: Mete Header com tipos

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+/// <reference types="vite-plugin-svgr/client" />
+
+import { useState } from 'react'
 
 import ReactGA from 'react-ga4'
 import Headroom from 'react-headroom'
@@ -8,55 +10,48 @@ import logo from '../img/logo so letras brancas.png'
 import Menu from '../svg/menu.svg?react'
 import './header.css'
 
-class Header extends React.Component {
-  constructor () {
-    super()
-    this.state = { openMenu: false }
-    this.handleClickMenuButton = this.handleClickMenuButton.bind(this)
-    this.handleClickLink = this.handleClickLink.bind(this)
-  }
+export default function Header () {
+  const [openMenu, setOpenMenu] = useState(false)
 
-  handleClickMenuButton () {
+  const handleClickMenuButton = () => {
     ReactGA.event({
       category: 'NavMenuToogle', // Required
-      action: `Menu ${this.state.openMenu ? 'fecha' : 'abre'}` // Required
+      action: `Menu ${openMenu ? 'fecha' : 'abre'}` // Required
     })
-    this.setState({ openMenu: !this.state.openMenu })
+    setOpenMenu(!openMenu)
   }
 
-  handleClickLink (page) {
+  const handleClickLink = (page: string) => {
     ReactGA.event({
       category: 'NavMenuLink', // Required
       action: `Navega ${page}` // Required
     })
-    this.setState({ openMenu: false })
+    setOpenMenu(false)
   }
 
-  render () {
-    const menustatus = this.state.openMenu ? ' navmenu-open' : ''
-    return (
-      <Headroom>
-        <div className='navbar'>
-          <div className='navbar-content'>
-            <div className='navbar-delimiter'>
-              <div className='navtop'>
-                <div className='navtop-side' />
-                <NavLink className='navimg' end to='/' onClick={() => this.handleClickLink('logo')}>
-                  <img src={logo} alt='QueerIST logo' />
-                </NavLink>
-                <Menu className='navtop-side' onClick={this.handleClickMenuButton} />
-              </div>
-              <NavLink className={'navlink' + menustatus} to='/sobre' onClick={() => this.handleClickLink('sobre')}>Sobre</NavLink>
-              <NavLink className={'navlink' + menustatus} to='/eventos' onClick={() => this.handleClickLink('eventos')}>Eventos</NavLink>
-              <NavLink className={'navlink' + menustatus} to='/projetos/' onClick={() => this.handleClickLink('projetos')}>Projetos</NavLink>
-              <a className={'navlink' + menustatus} href='https://queerist.tecnico.ulisboa.pt/blog/' onClick={() => this.handleClickLink('blog')}>Blog</a>
-              <a className={'navlink' + menustatus} href='http://eepurl.com/dnApYP' onClick={() => this.handleClickLink('newsletter')}>Newsletter</a>
+  const handleClickLinkCb = (page: string) => () => { handleClickLink(page) }
+
+  const menustatus = openMenu ? ' navmenu-open' : ''
+  return (
+    <Headroom>
+      <div className='navbar'>
+        <div className='navbar-content'>
+          <div className='navbar-delimiter'>
+            <div className='navtop'>
+              <div className='navtop-side' />
+              <NavLink className='navimg' end to='/' onClick={handleClickLinkCb('logo')}>
+                <img src={logo} alt='QueerIST logo' />
+              </NavLink>
+              <Menu className='navtop-side' onClick={handleClickMenuButton} />
             </div>
+            <NavLink className={'navlink' + menustatus} to='/sobre' onClick={handleClickLinkCb('sobre')}>Sobre</NavLink>
+            <NavLink className={'navlink' + menustatus} to='/eventos' onClick={handleClickLinkCb('eventos')}>Eventos</NavLink>
+            <NavLink className={'navlink' + menustatus} to='/projetos/' onClick={handleClickLinkCb('projetos')}>Projetos</NavLink>
+            <a className={'navlink' + menustatus} href='https://queerist.tecnico.ulisboa.pt/blog/' onClick={handleClickLinkCb('blog')}>Blog</a>
+            <a className={'navlink' + menustatus} href='http://eepurl.com/dnApYP' onClick={handleClickLinkCb('newsletter')}>Newsletter</a>
           </div>
         </div>
-      </Headroom>
-    )
-  }
+      </div>
+    </Headroom>
+  )
 }
-
-export default Header

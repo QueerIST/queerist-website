598441d1870c4227261bbbaa99b155417b8afa40
Francisco Sousa <francisco.sousa.98@tecnico.ulisboa.pt>
2024-03-17 14:41:33 +0000

    :bulb: Adiciona tipos para env vars

---
+++ src/vite-env.d.ts
@@ -0,0 +1,12 @@
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_FULL_URL: string
+  readonly VITE_BASE_URL: string
+  readonly VITE_GA_CODE?: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}

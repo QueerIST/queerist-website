60e61154c6e26d44a3493469c55affb08caa9118
Francisco Sousa <francisco.sousa.98@tecnico.ulisboa.pt>
2024-05-12 18:18:26 +0100

    :sparkles: Faz EventTile ter inline ou não

--- src/components/EventTile.tsx
+++ src/components/EventTile.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 
-import { useLocation } from 'react-router-dom'
+import { NavLink, useLocation } from 'react-router-dom'
 
 import Expand from './../svg/expand.svg?react'
 import Button from './Button'
@@ -10,27 +10,34 @@ import { type Event } from '../types/domain'
 
 import './tile.css'
 
-const EventTile = ({ data, n }: { data: Event, n: number }) => {
+const EventTile = ({ data, n, inline = false }: { data: Event, n: number, inline?: boolean }) => {
   const { id, name, description, imgLink, logoLink, bgColor, textColor, happenings, seeMoreText } = data
 
   const [open, setOpen] = useState(false)
   const location = useLocation()
   const dir = n % 2 !== 0 ? 'left' : 'right'
-  const openClass = open ? 'open' : ''
+  const openClass = inline && open ? 'open' : ''
 
   const EventTypeInfoButton = (seeMoreText?: string) => (
-    <button
+    inline
+      ? <button
       onClick={() => { setOpen(!open) }}
       className={`tile-info-b tile-info-button ${openClass}`}
     >
-      {seeMoreText ?? 'Ver mais'}
-      <Expand fill={textColor} />
-    </button>
+        {seeMoreText ?? 'Ver mais'}
+        { <Expand fill={textColor} />}
+      </button>
+      : <NavLink
+      className='tile-info-b'
+      to={{ pathname: `${id}` }}
+    >
+        {seeMoreText}
+      </NavLink>
   )
 
   useEffect(() => {
-    if (location.hash.split('-')[0] === `#${id}`) { setOpen(true) }
-  }, [location, id])
+    if (inline && location.hash.split('-')[0] === `#${id}`) { setOpen(true) }
+  }, [location, id, inline])
 
   return (
     <div id={id}>
@@ -45,13 +52,13 @@ const EventTile = ({ data, n }: { data: Event, n: number }) => {
           <h3 className='tile-info-text-text'>{name}</h3>
           <p className='tile-info-text-desc tile-info-text-text'>{description}</p>
           {happenings !== undefined &&
-          <Button actionComp='EventTile' actionName={`Clica ${name}`} actionLabel={open ? 'Close' : 'Open'} borderColor={textColor} color={textColor}>
+          <Button actionComp='EventTile' actionName={`Clica ${name}`} actionLabel={inline ? (open ? 'Close' : 'Open') : undefined} borderColor={textColor} color={textColor}>
             {EventTypeInfoButton(seeMoreText)}
           </Button>}
         </div>
         <div className='tile-info-img'>
           <img className={openClass} src={publicPath(imgLink)} alt={name} />
-          {happenings !== undefined && <EventGallery open={open} data={happenings} />}
+          {inline && happenings !== undefined && <EventGallery open={open} data={happenings} />}
         </div>
       </div>
     </div>

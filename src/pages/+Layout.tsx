9e5ea77221f15d852a804e2e695007b4e97ff0b2
Francisco Sousa <francisco.sousa.98@tecnico.ulisboa.pt>
2024-11-02 12:22:26 +0000

    :children_crossing: Migra código só para depois de hidratação

--- src/pages/+Layout.tsx
+++ src/pages/+Layout.tsx
@@ -3,29 +3,12 @@
 
 import React, { type PropsWithChildren } from 'react'
 
-import AOS from 'aos'
-import ReactGA from 'react-ga4'
-
 import { Footer } from '../components/Footer'
 import { Header } from '../components/Header'
 
 import '../components/layout.css'
 import 'aos/dist/aos.css'
 import '../webfontkit/fontstylesheet.css'
-
-AOS.init({
-  once: true
-})
-
-if (import.meta.env.PROD && import.meta.env.MODE !== 'github') {
-  if (import.meta.env.VITE_GA_CODE !== '') {
-    ReactGA.initialize(import.meta.env.VITE_GA_CODE, {
-      gaOptions: {
-        siteSpeedSampleRate: 100
-      }
-    })
-  }
-}
 
 export function Layout ({ children }: PropsWithChildren) {
   return (

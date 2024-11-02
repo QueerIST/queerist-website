296c7404ea9f8b63b452250512be5609fc0ae330
Francisco Sousa <francisco.sousa.98@tecnico.ulisboa.pt>
2024-11-02 12:21:06 +0000

    :heavy_plus_sign: Adiciona vite-plugin-cjs-interop para corrigir packages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import react from '@vitejs/plugin-react-swc'
 import vike from 'vike/plugin'
 import { loadEnv } from 'vite'
 import checker from 'vite-plugin-checker'
+import { cjsInterop } from 'vite-plugin-cjs-interop'
 import svgr from 'vite-plugin-svgr'
 
 export default function defineConfig ({ mode }: { mode: string }) {
@@ -14,7 +15,7 @@ export default function defineConfig ({ mode }: { mode: string }) {
     // Include React plugin to serve `/@react-refresh` and transform HTML to
     // inject this dependency.
       react(),
-      vike({ prerender: true }),
+      vike({ prerender: false }),
       svgr({
         svgrOptions: {
           plugins: ['@svgr/plugin-svgo', '@svgr/plugin-jsx'],
@@ -29,6 +30,14 @@ export default function defineConfig ({ mode }: { mode: string }) {
           // for example, lint .ts and .tsx
           lintCommand: 'eslint . --ext ts,tsx'
         }
+      }),
+      cjsInterop({
+        // Add broken npm package here
+        dependencies: [
+          // Apply patch to root import:
+          'react-headroom',
+          'react-helmet-async'
+        ]
       })
     ],
     base: env.VITE_BASE_URL

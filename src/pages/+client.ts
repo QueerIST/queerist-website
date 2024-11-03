53167ab7b49befff6448d9efb92a9620fa0902b4
Francisco Sousa <francisco.sousa.98@tecnico.ulisboa.pt>
2024-11-03 02:22:33 +0000

    :rewind: Tira código extra

--- src/pages/+client.ts
+++ src/pages/+client.ts
@@ -1,21 +1,16 @@
 import AOS from 'aos'
 import ReactGA from 'react-ga4'
-import type { OnHydrationEndAsync } from 'vike/types'
 
-export const onHydrationEnd: OnHydrationEndAsync = async (
-  _pageContext
-): ReturnType<OnHydrationEndAsync> => {
-  AOS.init({
-    once: true
-  })
+AOS.init({
+  once: true
+})
 
-  if (import.meta.env.PROD && import.meta.env.MODE !== 'github') {
-    if (import.meta.env.VITE_GA_CODE !== '') {
-      ReactGA.initialize(import.meta.env.VITE_GA_CODE, {
-        gaOptions: {
-          siteSpeedSampleRate: 100
-        }
-      })
-    }
+if (import.meta.env.PROD && import.meta.env.MODE !== 'github') {
+  if (import.meta.env.VITE_GA_CODE !== '') {
+    ReactGA.initialize(import.meta.env.VITE_GA_CODE, {
+      gaOptions: {
+        siteSpeedSampleRate: 100
+      }
+    })
   }
 }

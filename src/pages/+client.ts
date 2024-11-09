59b8ae095d42f382e905b41c78a8fb450298d50b
Francisco Sousa <francisco.sousa.98@tecnico.ulisboa.pt>
2024-11-09 23:22:05 +0000

    :green_heart: Remove actions de Github

--- src/pages/+client.ts
+++ src/pages/+client.ts
@@ -6,14 +6,12 @@ AOS.init({
   once: true
 })
 
-if (import.meta.env.PROD && import.meta.env.MODE !== 'github') {
-  if (import.meta.env.VITE_GA_CODE !== '') {
-    ReactGA.initialize(import.meta.env.VITE_GA_CODE, {
-      gaOptions: {
-        siteSpeedSampleRate: 100
-      }
-    })
-  }
+if (import.meta.env.PROD && import.meta.env.VITE_GA_CODE !== '') {
+  ReactGA.initialize(import.meta.env.VITE_GA_CODE, {
+    gaOptions: {
+      siteSpeedSampleRate: 100
+    }
+  })
 }
 
 TagManager.initialize({

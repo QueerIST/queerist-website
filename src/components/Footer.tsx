b6b4243e0a5b8fe81210299ad27220497d567ecc
Francisco Sousa <francisco.sousa.98@tecnico.ulisboa.pt>
2025-04-19 23:53:22 +0100

    :fire: Remove extra css class

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,11 +9,11 @@ export function Footer () {
   return (
     <footer>
       <div className='footer-logos'>
-        <a id='instagram-link' href='https://instagram.com/queer.ist'><Instagram className='footer-logo clickable' /></a>
-        <a id='facebook-link' href='https://facebook.com/QueerIST'><Facebook className='footer-logo clickable' /></a>
-        <a id='twitter-link' href='https://twitter.com/QueerIST'><Twiter className='footer-logo clickable' /></a>
-        <a id='github-link' href='https://github.com/QueerIST'><GitHub className='footer-logo clickable' /></a>
-        <a id='email-link' href='mailto:queerist.sa@aeist.pt'><Email className='footer-logo clickable' /></a>
+        <a id='instagram-link' href='https://instagram.com/queer.ist'><Instagram className='footer-logo' /></a>
+        <a id='facebook-link' href='https://facebook.com/QueerIST'><Facebook className='footer-logo' /></a>
+        <a id='twitter-link' href='https://twitter.com/QueerIST'><Twiter className='footer-logo' /></a>
+        <a id='github-link' href='https://github.com/QueerIST'><GitHub className='footer-logo' /></a>
+        <a id='email-link' href='mailto:queerist.sa@aeist.pt'><Email className='footer-logo' /></a>
       </div>
       <div className='footer-text'>
         <address>

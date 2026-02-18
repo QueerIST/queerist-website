f9b0bf2e23a38800fa89258413ffcd8ab3bab04e
Francisco Sousa <francisco.sousa.98@tecnico.ulisboa.pt>
2026-02-18 22:59:53 +0000

    :chart_with_upward_trend: Normaliza mailchimp UTM parameters

--- src/pages/+client.ts
+++ src/pages/+client.ts
@@ -7,6 +7,29 @@ AOS.init({
 
 const params = new URLSearchParams(location.search)
 
+function normalizeUTMParameters () {
+  const utmSource = params.get('utm_source')
+
+  if (utmSource === 'QueerIST') {
+    params.set('utm_source', 'mailchimp')
+    params.set('utm_medium', 'email')
+
+    const utmCampaign = params.get('utm_campaign')
+    if (utmCampaign) {
+      const index = utmCampaign.indexOf('-')
+      if (index !== -1) {
+        const campaign = utmCampaign.substring(index + 1)
+        params.set('utm_campaign', campaign)
+      }
+    }
+
+    const newUrl = `${location.pathname}?${params.toString()}${location.hash}`
+    window.history.replaceState({}, '', newUrl)
+  }
+}
+
+normalizeUTMParameters()
+
 if (import.meta.env.VITE_GA_CODE !== '') {
   let debugMode
 
